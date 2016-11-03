2735d94f5838819534cfcb70e243f4c809bb7990
Reza Ranjkesh Shoorkaii <reza@Rezas-MBP.lan>
2016-11-03 15:53:20 +0330

    interval gallery

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
  * Created by reza on 11/3/16.
  */
 
+var $div = '<div class="content delete"></div>';
 $('#text').css('background-color','blue');
 
 var my_text = $('#my_text');
@@ -23,4 +24,20 @@ $('#text').click(function () {
 
 $('.thumb').click(function () {
     $('.big-img').attr('src', $(this).attr('src'));
+});
+
+var img_array = ['img/above-adventure-aerial-air.jpg', 'img/black-and-white-city-skyline-buildings.jpg'];
+var t = 0;
+setInterval(function () {
+    t ++;
+    $('.big-img').attr('src', img_array[t % 2]);
+}, 2000);
+
+$('.plus').click(function () {
+    $('.holder').append($div);
+    $('.content').addClass('border');
+});
+
+$('.delete').click(function () {
+    $(this).hide();
 });
