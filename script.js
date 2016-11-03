a4d9d42db4f081a14e8a2e0fe5fb9f9e98a27c61
Reza Ranjkesh Shoorkaii <reza@Rezas-MBP.lan>
2016-11-03 14:04:06 +0330

    new js

---
+++ script.js
@@ -0,0 +1,16 @@
+/**
+ * Created by reza on 11/3/16.
+ */
+
+// var t = document.getElementById('text').innerHTML;
+// document.getElementById('my_text').innerHTML = t;
+// console.log(t);
+
+// var cnt = 0;
+//
+// function salam() {
+//     cnt = cnt + 1;
+//     document.getElementById('my_text').innerHTML = cnt;
+// }
+
+document.getElementById('text').setAttribute('style', ('background-color :red;'));
