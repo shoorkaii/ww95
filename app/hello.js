bae5ccd0be1f148d2a5b24ae91446c6512329413
Reza <shoorkaii@gmail.com>
2016-11-10 10:08:46 +0330

    npm init

---
+++ app/hello.js
@@ -0,0 +1,24 @@
+/**
+ * Created by reza on 11/10/16.
+ */
+
+var fs = require('fs');
+var m = require('./module');
+
+// fs.readFile('st.txt', function (err, inputText) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log(inputText.toString());
+//     }
+// })
+
+
+var hello = function () {
+    console.log(m);
+}
+
+
+hello();
+
---
+++ app/module.js
@@ -0,0 +1,17 @@
+/**
+ * Created by reza on 11/10/16.
+ */
+
+var sum = function (a , b) {
+    return a + b;
+}
+
+var print = function () {
+    return sum(10, 2);
+}
+
+
+
+
+
+module.exports = print();
